6357eb35d36b114b4b53abcf3f348b89103b4ce4
Alex Robert <contact.alexrobert04@gmail.com>
2024-10-21 18:30:23 +0200

    💬 Text presentation

--- src/Sections/Presentation.tsx
+++ src/Sections/Presentation.tsx
@@ -5,15 +5,16 @@ function Presentation({ className }: { className: string }) {
     <Section className={className}>
       <>
         <h2>Bienvenue sur mon portfolio !</h2>
-        <p>
-          Développeur autodidacte avec plus de 12 ans d'expérience, je suis
-          passionné par la création de solutions techniques innovantes et
-          efficaces. Ma philosophie repose sur l'apprentissage continu,
-          l'expérimentation et la réalisation de projets concrets. Vous
-          trouverez ici une sélection de mes travaux en développement
-          informatique, allant de projets web à des solutions logicielles
-          complexes. Explorez mes projets et n'hésitez pas à me contacter pour
-          discuter de collaborations ou d'opportunités.
+        <p className="text-muted_foreground">
+          Bonjour, je m'appelle Alex Robert, j'ai 22 ans et je suis passionné
+          par la programmation depuis mes 10 ans, en particulier en C et en
+          assembleur x86. Bien que je sois encore en apprentissage sur l'ASM
+          x86, j'adore explorer le bas niveau et en découvrir toutes ses
+          subtilités. Je suis constamment à la recherche de nouveaux défis, car
+          pour moi, coder, c'est repousser les limites et explorer de nouvelles
+          possibilités. Mon ambition est de participer à des projets innovants,
+          aux côtés d'autres passionnés, pour créer quelque chose
+          d'exceptionnel.
         </p>
       </>
     </Section>
